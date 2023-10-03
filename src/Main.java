42520d1efdb1db60c35f2a735ef3df6438b6b117
ZaizAU93 <zajtz.andrey1993@mail.ru>
2023-10-03 09:59:48 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+import java.io.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        //«адача 1:
+        //»спользу€ FileOutputStream и FileInputStream записать в файл tms.txt строку, введенную
+        //пользователем с консоли. —читать строку из файла tms.txt и вывести в консоль, заменив
+        //все пробелы на знак нижнего подчеркивани€.
+        System.out.println("¬ведите строку дл€ записи в файл");
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        File file = new File("E:\\TeachMeSkills\\HomeWork_12\\tms.txt");
+
+        FileOutputStream fOs = new FileOutputStream(file, true);
+        fOs.write(text.getBytes());
+
+
+
+        String strings = "stroka нова€";
+        try (FileWriter fileWriter = new FileWriter(file)) {
+           fileWriter.write(String.valueOf(strings.getBytes()));
+           fileWriter.flush();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+        try {
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            int i;
+            String s = "";
+            while (bufferedReader.read() != -1) {
+                i = (bufferedReader.read());
+                System.out.print((char) i);
+            }
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
