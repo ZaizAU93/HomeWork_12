5aeb2f1e44a90c2467e1d3958d54d6e287ff15ac
ZaizAU93 <zajtz.andrey1993@mail.ru>
2023-10-13 15:55:05 +0300

    1

--- src/ValidContr/Validity.java
+++ src/ValidContr/Validity.java
@@ -1,54 +1,46 @@
 package ValidContr;
 
-public class Validity {
-    private String[] docnum;
+import java.util.Arrays;
 
-    public Validity(String[] docnum) {
-        this.docnum = docnum;
-    }
+public class Validity {
+    public String strResult = "";
 
     public String[] getDocnum() {
         return docnum;
     }
 
-    public void setDocnum(String[] docnum) {
+    public  String[] docnum;
+    public String string = "%s - Невалидный контракт";
+    public Validity(String[] docnum) {
         this.docnum = docnum;
     }
 
-    public boolean validity_Check_SpecialCharacters(String[] docnum) {
-        String SpecialCharachters = "[!@#$%^&*(),.?\":{}|<>]";
-        for (int i = 0; i < docnum.length; i++)
-            for (int j = 0; j < docnum[j].length(); j++) {
-                if (docnum[i].contains(SpecialCharachters)){
-                    System.out.println("Несоответсвие служебным символам");
+
+    public boolean validity_Check_SpecialCharacters(String str) {
+            for (char c : str.toCharArray()) {
+                if (!(Character.isLetter(c) || Character.isDigit(c) || c == '\r')) {
+                    return true;
                 }
             }
-        return true;
+        return false;
     }
 
 
-    public boolean validity_Check_Length(){
-        boolean isValid = true;
-        int k = 0;
-        for(int i = 0; i < docnum.length; i++){
-            if (docnum[i].length() != 15){
-                System.out.println(" Несоответсвие длинне"+ docnum[i]);
-                isValid = false;
-            k++;
+
+    public boolean validity_Check_Length(String str){
+            if (str.trim().length() != 15){
+                return true;
             }
-        }
-        System.out.println( "счетчик "+k);
-        return isValid;
+        return false;
     }
 
-    public boolean validity_Check_Name(String[] docnum){
+
+    public boolean validity_Check_Name(String str){
         String doc = "docnum";
         String contract = "contract";
-        for(int i = 0; i < docnum.length; i++){
-            if (docnum[i].substring(0,6) != doc || docnum[i].substring(0,9) != contract){
-                System.out.println("Невалидное название документа");
+            if (!(str.substring(0,6).equals(doc) || str.substring(0,8).equals(contract))){
+                return true;
             }
-        }
         return false;
     }
 
--- src/ValidContr/main.java
+++ src/ValidContr/main.java
@@ -3,6 +3,7 @@ package ValidContr;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -21,35 +22,55 @@ public class main {
         //следует записать в другой файл-отчет, но после номеров документов следует добавить
         //информацию о том, почему этот документ не валиден
 
-        String catalog = "E:\\TeachMeSkills\\HomeWork_12\\";
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите название файла, через точку расши рение .txt");
+        String catalog = "E:\\TeachMeSkills\\HomeWork_12\\docum.txt";
+        //Scanner scanner = new Scanner(System.in);
+        //System.out.println("Введите название файла, через точку расши рение .txt");
         //catalog.concat(scanner.nextLine());
-        catalog += scanner.nextLine();
+        //catalog += scanner.nextLine();
         String text = "";
         System.out.println(catalog);
         try {
             FileInputStream fis = new FileInputStream(catalog);
             int i;
-            while ((i=(fis.read())) != -1){
-                text += (char)i;
+            while ((i = (fis.read())) != -1) {
+                text += (char) i;
             }
 
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
         String[] arr = text.split("\\n");
-        System.out.println("длинна масива "+arr.length);
 
-        for (String arrs: arr) {
-        //    System.out.println(arrs);
+
+        // System.out.println(Arrays.toString(text.split("\t")));
+        try {
+            FileOutputStream fos = new FileOutputStream("E:\\TeachMeSkills\\HomeWork_12\\noValid.txt");
+            Validity val = new Validity(arr);
+
+            for (String s : arr) {
+                String strResult = "";
+                if (val.validity_Check_Name(String.valueOf(s))) {
+                    strResult = "Название не соответствеет условиям " + s + '\r';
+                    fos.write(strResult.getBytes(StandardCharsets.UTF_8));
+                } else if (val.validity_Check_SpecialCharacters(String.valueOf(s))) {
+                    strResult = "В названиии документа имеются служебные символы " + s + '\r';
+                    fos.write(strResult.getBytes(StandardCharsets.UTF_8));
+                } else if (val.validity_Check_Length(String.valueOf(s))) {
+                    strResult = "Не соответсвтвие длинны " + s + '\r';
+                    fos.write(strResult.getBytes(StandardCharsets.UTF_8));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
+        Validity k = new Validity(arr);
+        for (String s: arr){
+          //  k.validity_Check_Length(s);
+         // k.validity_Check_Name(s);
+          //  k.validity_Check_SpecialCharacters(s);
+        }
 
-       // System.out.println(Arrays.toString(text.split("\t")));
-
-       Validity val = new Validity(arr);
-        val.validity_Check_Length();
     }
 }
